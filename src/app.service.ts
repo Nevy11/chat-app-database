eb8744a08be4c52eed0dd36ebecfe05308757e3c
nevy11 <smongare2003@gmail.com>
2024-08-05 08:26:03 +0300

    connect typeOrm to database

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Congrats Steve @smart_coder11 @nevy11';
   }
 }
